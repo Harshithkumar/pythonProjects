a5a19dd50a997c75c4f37f81aa2c2330da0df3ea
hakumar <harsh.rovi@gmail.com>
2023-02-28 20:31:18 +0530

    Modified dict_covid_cases file

--- Checks/dict_covid_Cases.py
+++ Checks/dict_covid_Cases.py
@@ -1,31 +1,44 @@
 import json
 import requests
 import pandas
+import pytest
 
-URL = 'https://api.covid19api.com/summary'
+class Test_matchConfirmedCases:
 
-response_data = requests.get(URL)
-parsed_data = json.loads(response_data.text)
-print(parsed_data)
+    URL = 'https://api.covid19api.com/summary'
 
-global_confirmed_Cases = int(parsed_data['Global']['TotalConfirmed'])
-print('Global_Confirmed_Cases = ', global_confirmed_Cases)
+    @pytest.mark.GetURL
+    def test_getResponse(self):
+        response_data = requests.get(self.URL)
+        parsed_data = json.loads(response_data.text)
+        print(parsed_data)
+        return parsed_data
 
-list_of_countries = []
-cn_total_confirmedcases = 0
-for cn_info in parsed_data['Countries']:
-    each_cn = cn_info['Country']
-    total_confirmed_Cases = cn_info['TotalConfirmed']
-    list_of_countries.append([each_cn, total_confirmed_Cases])
-    cn_total_confirmedcases += int(total_confirmed_Cases)
-print('list_of_countries = ', list_of_countries)
-print('cn_total_confirmed_cases =', cn_total_confirmedcases)
+    def test_getGlobalConfirmedCases(self):
+        global_parsed_data = Test_matchConfirmedCases.test_getResponse(self)
+        global_confirmed_Cases = int(global_parsed_data['Global']['TotalConfirmed'])
+        print('Global_Confirmed_Cases = ', global_confirmed_Cases)
+        return global_confirmed_Cases
 
-data_frame = pandas.DataFrame(data=list_of_countries, columns=['each_cn','total_confirmed_Cases'])
+    def test_Calculate_confirmed_cases(self):
+        list_of_countries = []
+        cn_total_confirmedcases = 0
+        summary_data = Test_matchConfirmedCases.test_getResponse(self)
+        for cn_info in summary_data['Countries']:
+            each_cn = cn_info['Country']
+            total_confirmed_Cases = cn_info['TotalConfirmed']
+            list_of_countries.append([each_cn, total_confirmed_Cases])
+            cn_total_confirmedcases += int(total_confirmed_Cases)
+            # print('list_of_countries = ', list_of_countries)
+        print('cn_total_confirmed_cases =', cn_total_confirmedcases)
+        return list_of_countries, cn_total_confirmedcases
 
-print(data_frame.head(10))
-
-assert global_confirmed_Cases == cn_total_confirmedcases, " Total cofirmed cases not matached"
+    def test_make_Table(self):
+        list_of_countries,cn_total_confirmedcases = Test_matchConfirmedCases.test_Calculate_confirmed_cases(self)
+        global_confirmed_Cases = Test_matchConfirmedCases.test_getGlobalConfirmedCases(self)
+        data_frame = pandas.DataFrame(data=list_of_countries, columns=['each_cn','total_confirmed_Cases'])
+        print(data_frame.head(10))
+        assert global_confirmed_Cases == cn_total_confirmedcases, " Total cofirmed cases not matached"
 
 
 
