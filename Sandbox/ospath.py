7d706aef4a90a43fb84da24eb2b712f3f68e3661
hakumarxperi <harshith.kumar@xperi.com>
2023-12-26 13:24:30 +0530

    os scripts

--- Sandbox/ospath.py
+++ Sandbox/ospath.py
@@ -1,10 +1,29 @@
 import os
-import sys
+# import sys
+#
+#
+# print(os.getcwd())
+#
+# print(os.chdir('/Users/hakumar/PycharmProjects/Experiments/BrowserFramework/Screenshots'))
+#
+# print(os.getcwd())
+#
+# ###################
+# directory = os.path.dirname(os.path.abspath(__file__))
+# print(directory)
+#
+# filename = "touch os_test.txt"
+# filepath = os.path.join(directory, filename)
+#
+# cmd = 'touch a_filename.txt'
+# os.system(cmd)
+#
+# print(filepath)
+# with open(filepath, 'w') as file:
+#     file.write("")
 
 
-print(os.getcwd())
-
-print(os.chdir('/Users/hakumar/PycharmProjects/Experiments/BrowserFramework/Screenshots'))
-
-print(os.getcwd())
+result = os.scandir()
+for x in result:
+    print(x)
 
